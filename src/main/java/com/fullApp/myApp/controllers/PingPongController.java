f879ae3d325125683850dc486d3d8680a33a65ae
nikitosik-761 <ndlysenko2004@gmail.com>
2024-03-27 21:18:41 +0300

    cd workflow with new controller

---
+++ src/main/java/com/fullApp/myApp/controllers/PingPongController.java
@@ -0,0 +1,16 @@
+package com.fullApp.myApp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+record PingPong(String ping){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong(){
+          return new PingPong("Test Ping Pong");
+    }
+
+}
